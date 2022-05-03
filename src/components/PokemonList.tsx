d44d46131d0fb185bf9a89868c3adb3b53bd2094
saulChavin <sagus_97@hotmail.com>
2022-05-03 01:28:02 -0500

    Colors on cards Grid Whole app slate background

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,25 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import pokemonDB from '../api/pokemonDB';
-import { Result, PokemonDBResponse } from '../interfaces/pokemonInterface';
+import { Result, PokemonDBResponse, Pokemon } from '../interfaces/pokemonInterface';
 import { PokemonCard } from './PokemonCard';
 
 export const PokemonList = () => {
 
-    const [pokeList, setPokeList] = useState<Result[]>([]);
-
+    const [pokeList, setPokeList] = useState<Pokemon[]>([]);
+    
     useEffect(() => {
-        (async () => {
-            const pokemonListRes: PokemonDBResponse = await pokemonDB.fetchApi('/pokemon');
-            const results: Result[] = pokemonListRes.results;
-            setPokeList(results);
-        })();
+        pokemonDB.fetchApi<PokemonDBResponse>('/pokemon')
+        .then(({results}) => {
+            console.log('results', results)
+            results.forEach(pokemon => {
+                pokemonDB.fetchApi<Pokemon>(`/pokemon/${pokemon.name}`)
+                .then(res => {
+                    setPokeList(prev => [...prev, res])
+                })
+            })
+        })
+        .catch(err => console.log('err', err));
     }, []);
 
     return (
-        <div className='flex flex-wrap gap-1 justify-between'>
+        <div className='grid grid-cols-3 gap-8 bg-gray-900 p-16'>
             {
                 pokeList.map(pokemon => {
-                    return <PokemonCard key={pokemon.name} name={pokemon.name} />
+                    return <PokemonCard key={pokemon.name} pokemon={pokemon} />
                 })
             }
 
